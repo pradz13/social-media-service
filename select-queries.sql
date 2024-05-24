0bce8833c45afb20f308d4a7a6adc25f9dffafe1
Pradipta <pradipta.nag2@gmail.com>
2024-05-24 21:14:11 +0530

    Create select-queries.sql

---
+++ select-queries.sql
@@ -0,0 +1,18 @@
+USE social_media_db;
+-- Select skills for a particular users
+select u.name 'Name' , s.name 'Skill'
+from USER_SKILLS us 
+INNER JOIN USER_PROFILE u
+ON us.user_id = u.id
+INNER JOIN SKILLS s
+on us.skill_id = s.id
+where u.id = 1;
+
+-- Select the count of skills for users
+select u.name 'Name', count(s.name) 'Skill Count'
+from USER_SKILLS us 
+INNER JOIN USER_PROFILE u
+ON us.user_id = u.id
+INNER JOIN SKILLS s
+on us.skill_id = s.id
+GROUP BY u.name;
