7c3381c89a0c630f1e2e08c83ed42fff4f5d22a8
Pradipta <pradipta.nag2@gmail.com>
2024-05-24 20:30:22 +0530

    Create create-table-scripts.sql
    Create table scripts

---
+++ create-table-scripts.sql
@@ -0,0 +1,24 @@
+USE social_media_db;
+CREATE TABLE USER_PROFILE(
+	id int NOT NULL,
+    profile_picture_uri varchar(255) NOT NULL,
+    name varchar(255) NOT NULL,
+    email varchar(255) NOT NULL,
+    phone_number int NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE SKILLS(
+	id int NOT NULL,
+    name varchar(255) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE USER_SKILLS(
+	id int NOT NULL,
+    user_id int NOT NULL,
+    skill_id int NOT null,
+    PRIMARY KEY(id),
+    CONSTRAINT FK_USER FOREIGN KEY (user_id) REFERENCES USER_PROFILE(id),
+    CONSTRAINT FK_SKILLS FOREIGN KEY (skill_id) REFERENCES SKILLS(id)
+);
